8bc4fca498e457c21827077277d480651bdfd1dc
Carlos Gonzalez <carlosegl10@hotmail.com>
2022-05-31 22:48:06 -0400

    add app

--- pokemon_backend/pokemon_backend/settings.py
+++ pokemon_backend/pokemon_backend/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pokemon',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
